7c1936c7a441cd284b8b1bb7bbc0be7ca0546972
Antonio <antoniorg.129@gmail.com>
2023-07-27 10:51:14 +0200

    Primer commit

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -54,7 +54,7 @@ export class GifsService {
 
     const params = new HttpParams()
       .set('api_key', this.apiKey)
-      .set('limit', 10)
+      .set('limit', 20)
       .set('q',tag)
 
     this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params })
